504907804f9882a1d880b227ae463bae5cadd15a
Christoph Reimers <christoph@superservice-international.com>
2014-12-04 09:16:53 +0000

    added start gallery to cms_plugins.py

--- cms_plugins.py
+++ cms_plugins.py
@@ -23,4 +23,21 @@ class GalleryPlugin(CMSPluginBase):
 
         return context
 
+
+class StartSliderPlugin(CMSPluginBase):
+
+    model = GalleryPluginModel
+    name = _("Start Slider")
+    render_template = "djangocms_gallerij/_start_image_slider.html"
+
+    def render(self, context, instance, placeholder):
+        images = instance.gallery.images.all()
+        context.update({
+            'image_list': list(images),
+            'start_slide': 0,
+        })
+
+        return context
+
 plugin_pool.register_plugin(GalleryPlugin)
+plugin_pool.register_plugin(StartSliderPlugin)
